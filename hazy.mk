33aa2d9aab6e83735a43cb709db87e2c7ddd3800
RolanDroid <rolandosimoncelli8@gmail.com>
2015-09-14 16:13:25 +0200

    Hazy support
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- hazy.mk
+++ hazy.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common HAZY stuff.
+$(call inherit-product, vendor/hazy/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := cm_thea
+PRODUCT_NAME := hazy_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
